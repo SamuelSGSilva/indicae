769687b2338dd74c2e20a0cdd0e363b679fd8571
[dyad] <git@dyad.sh>
2025-10-26 18:45:13 -0300

    [dyad] Removi o ícone de sino da tela de criação de perfil - wrote 1 file(s)

--- components/CreateProfileScreen.tsx
+++ components/CreateProfileScreen.tsx
@@ -104,7 +104,7 @@ const CreateProfileScreen: React.FC<CreateProfileScreenProps> = ({ user, onBack,
        <header className="p-4 flex justify-between items-center flex-shrink-0">
         <button onClick={onBack} className="text-white">{icons.back('w-6 h-6')}</button>
         <h1 className="text-xl font-bold text-white">Criar Meu Perfil</h1>
-        <button className="text-white">{icons.bell('w-6 h-6')}</button>
+        <div className="w-6 h-6"></div> {/* Placeholder para manter o espaçamento */}
       </header>
 
       <main className="flex-1 flex flex-col">
